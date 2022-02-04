ae38a4eec48cb637362c71e7296b179a643f1e98
Jitendra <jeetscmaker@gmail.com>
2022-02-05 00:11:48 +0530

    Application file name refactored.

--- api-gateway/src/main/java/org/jk/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/jk/ApiGatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class DemoApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
